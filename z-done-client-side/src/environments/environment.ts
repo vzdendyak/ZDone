31839688be5b101e54b2c120df82310ed56db593
Vasyl <vzdendyak@gmail.com>
2020-06-29 21:58:45 +0300

    Ready to deploy. Fixed connection strings/urls

--- z-done-client-side/src/environments/environment.ts
+++ z-done-client-side/src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  basicUrl: 'https://localhost:5001/api',
-  url: 'http://localhost:4200',
+  // basicUrl: 'https://localhost:5001/api',
+  basicUrl: 'https://zdonewebapi20200626161312.azurewebsites.net/api',
+  // url: 'http://localhost:4200',
+  url: 'https://zdonewebapi20200626161312.azurewebsites.net',
   months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 };
 
